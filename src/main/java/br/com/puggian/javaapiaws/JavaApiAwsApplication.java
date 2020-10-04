d27308fea6d89cba5adcada6c9128a5e3b364444
John Puggian <puggianjv@gmail.com>
2020-10-04 19:16:37 -0300

    sample endpoint deploy API container on AWS

---
+++ src/main/java/br/com/puggian/javaapiaws/JavaApiAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.puggian.javaapiaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/puggian/javaapiaws/JavaApiAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.puggian.javaapiaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
